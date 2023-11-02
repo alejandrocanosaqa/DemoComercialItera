1acf6ef7dd51bea44dfdc4e8f578acc16bcb93d6
alejandroc45 <125397241+alejandroc45@users.noreply.github.com>
2023-11-02 09:25:49 +0100

    Demo comercial cambios

--- Test Suites/DemoComercialItera/DemoComercialALLNavegadores.ts
+++ Test Suites/DemoComercialItera/DemoComercialALLNavegadores.ts
@@ -37,15 +37,5 @@
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/DemoComercialItera/DemoComercial</testSuiteEntity>
       </TestSuiteRunConfiguration>
-      <TestSuiteRunConfiguration>
-         <configuration>
-            <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
-            <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Safari</runConfigurationId>
-         </configuration>
-         <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/DemoComercialItera/DemoComercial</testSuiteEntity>
-      </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
